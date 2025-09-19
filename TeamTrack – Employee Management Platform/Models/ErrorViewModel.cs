c183dee3ba8e04b6287aef988800ec01a5b404f1
Rakibul Islam <rakibulcste15@gmail.com>
2025-09-20 01:46:18 +0600

    Add project files.

---
+++ TeamTrack – Employee Management Platform/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TeamTrack___Employee_Management_Platform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
